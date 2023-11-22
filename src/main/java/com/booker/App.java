a54c7ca02d5d3d497549b4a93cc35b28fbc85bf3
Snootic <kaik.eu19@hotmail.com>
2023-11-22 11:06:57 -0300

    remove: hello world

--- src/main/java/com/booker/App.java
+++ src/main/java/com/booker/App.java
@@ -5,10 +5,6 @@ import java.sql.PreparedStatement;
 
 import com.booker.migrations.CreateTableProduto;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) throws Exception
@@ -24,12 +20,12 @@ public class App
         String query = "INSERT INTO livros(titulo, autor, isbn, editora, genero, ano) values (?, ?, ?, ?, ?, ?)";
 
         preparedStatement = conn.prepareStatement(query);
-        preparedStatement.setString(0, "Titulo do Livro");
-        preparedStatement.setString(1, "Autor do Livro");
-        preparedStatement.setString(2, "ISBN do Livro");
-        preparedStatement.setString(3, "Editora do Livro");
-        preparedStatement.setString(4, "Genero do Livro");
-        preparedStatement.setString(5, "Ano do Livro");
+        preparedStatement.setString(1, "Titulo do Livro");
+        preparedStatement.setString(2, "Autor do Livro");
+        preparedStatement.setString(3, "ISBN do Livro");
+        preparedStatement.setString(4, "Editora do Livro");
+        preparedStatement.setString(5, "Genero do Livro");
+        preparedStatement.setString(6, "Ano do Livro");
 
         preparedStatement.execute();
 
