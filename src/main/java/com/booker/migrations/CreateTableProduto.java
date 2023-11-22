0cf3945832ecde09f7af77f42d0dae019c043f5d
Snootic <kaik.eu19@hotmail.com>
2023-11-22 11:07:54 -0300

    fix: table creation

--- src/main/java/com/booker/migrations/CreateTableProduto.java
+++ src/main/java/com/booker/migrations/CreateTableProduto.java
@@ -14,7 +14,7 @@ public class CreateTableProduto {
     
         PreparedStatement preparedStatement;
     
-        String query = "create if not exists table produtos (id NOT NULL AUTO_INCREMENT, titulo varchar(255) NOT NULL, autor varchar(255) NOT NULL, isbn varchar(255) NOT NULL, editora varchar(255) NOT NULL, genero varchar(255) NOT NULL, ano varchar(255) NOT NULL), PRIMARY KEY(id)";
+        String query = "create table if not exists livros (id int AUTO_INCREMENT, titulo varchar(255) NOT NULL, autor varchar(255) NOT NULL, isbn varchar(255) NOT NULL, editora varchar(255) NOT NULL, genero varchar(255) NOT NULL, ano varchar(255) NOT NULL, PRIMARY KEY(id));";
     
         preparedStatement = conn.prepareStatement(query);
         preparedStatement.execute();
