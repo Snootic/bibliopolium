44fd516c84a146fb41967c62feb1786de7e861ca
Snootic <kaik.eu19@hotmail.com>
2023-11-22 12:05:43 -0300

    feat: add menu

--- src/main/java/com/booker/App.java
+++ src/main/java/com/booker/App.java
@@ -1,25 +1,12 @@
 package com.booker;
 
-import com.booker.domain.LivroService;
-import com.booker.infrastructure.entities.Livro;
-import com.booker.infrastructure.migrations.CreateTableProduto;
+import com.booker.application.Menu;
 
 public class App 
 {
     public static void main( String[] args ) throws Exception
     {
-        CreateTableProduto createTableProduto = new CreateTableProduto();
-        createTableProduto.createProdutos();
-
-        LivroService service = new LivroService();
-        service.criarLivro();
-        Livro livro = service.resgatarLivro("ISBN do Livro");
-        System.out.println("Titulo: " + livro.getTitulo());
-        System.out.println("Autor: " + livro.getAutor());
-        System.out.println("ISBN: " + livro.getISBN());
-        System.out.println("Editora: " + livro.getEditora());
-        System.out.println("Genero: " + livro.getGenero());
-        System.out.println("Ano de lançamento: " + livro.getAno());
-
+        Menu menu = new Menu();
+        menu.mainMenu();
     }
 }
---
+++ src/main/java/com/booker/application/Menu.java
@@ -0,0 +1,55 @@
+package com.booker.application;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import com.booker.application.dto.LivroInputDTO;
+import com.booker.domain.LivroService;
+import com.booker.infrastructure.migrations.CreateTableProduto;
+
+public class Menu {
+    public void mainMenu() throws SQLException, Exception{
+        CreateTableProduto createTableProduto = new CreateTableProduto();
+        createTableProduto.createProdutos();
+
+        
+        Scanner sc = new Scanner(System.in);
+        Scanner scInt = new Scanner(System.in);
+
+        String nome, autor, isbn, editora, genero;
+        int ano;
+
+        LivroService service = new LivroService();
+
+        System.out.print("Digite o nome do livro: ");
+        nome = sc.nextLine();
+
+        System.out.print("Digite o autor do livro: ");
+        autor = sc.nextLine();
+
+        System.out.print("Digite o isbn do livro: ");
+        isbn = sc.nextLine();
+
+        System.out.print("Digite o editora do livro: ");
+        editora = sc.nextLine();
+
+        System.out.print("Digite o genero do livro: ");
+        genero = sc.nextLine();
+
+        System.out.print("Digite o ano do livro: ");
+        ano = scInt.nextInt();
+
+        service.criarLivro(new LivroInputDTO(nome, autor, isbn, editora, genero, String.valueOf(ano)));
+        LivroInputDTO livro = service.resgatarLivro(isbn);
+        System.out.println("Titulo: " + livro.getTitulo());
+        System.out.println("Autor: " + livro.getAutor());
+        System.out.println("ISBN: " + livro.getISBN());
+        System.out.println("Editora: " + livro.getEditora());
+        System.out.println("Genero: " + livro.getGenero());
+        System.out.println("Ano de lançamento: " + livro.getAno());
+
+        sc.close();
+        scInt.close();
+
+    }
+}
